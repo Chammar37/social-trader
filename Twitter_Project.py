55264037f97348f65c4b112b88821df19715c0f8
chammar37 <m.chami37@hotmail.com>
2021-10-22 13:00:43 -0400

    imported credential variables

--- Twitter_Project.py
+++ Twitter_Project.py
@@ -1,6 +1,6 @@
 import tweepy
 from tweepy import StreamListener
-import tweepy_credentials
+from  tweepy_credentials import API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET 
 
 # Getting Stream
 class MaxListener(StreamListener):
@@ -14,10 +14,10 @@ class MaxListener(StreamListener):
             return False
 
 # Keys from Twitter
-API_KEY = 'E6EnVEln9E4XubkgUNec5S87p'
-API_SECRET_KEY = 'Soo0CC2gusNpBJiE5SXXGcawclZC9hXWgY5LWDGze6KvNHBh9m'
-ACCESS_TOKEN = '1032147310304808961-DYpRYrehYu3o7bw4r1kvKRZ9HKxPHU'
-ACCESS_TOKEN_SECRET = 'EE3sJuZ1dBG9L9q8dzxAuD5Zv1sLOMNgDJxlc58u04DD6'
+# API_KEY = 'E6EnVEln9E4XubkgUNec5S87p'
+# API_SECRET_KEY = 'Soo0CC2gusNpBJiE5SXXGcawclZC9hXWgY5LWDGze6KvNHBh9m'
+# ACCESS_TOKEN = '1032147310304808961-DYpRYrehYu3o7bw4r1kvKRZ9HKxPHU'
+# ACCESS_TOKEN_SECRET = 'EE3sJuZ1dBG9L9q8dzxAuD5Zv1sLOMNgDJxlc58u04DD6'
 
 # Starting Stream
 class MaxStream():
